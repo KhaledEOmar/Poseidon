a60b997f96bc498e86c12c2951357a0c21a094c8
Khaled Omar <khaledeomar@gmail.com>
2017-01-11 18:07:17 -0500

    added partial views, and imgs

--- src/js/poseidon.js
+++ src/js/poseidon.js
@@ -1,9 +1,35 @@
-$(document).ready(function(){
-	$('a').click(function(e){
-		alert('You clicked something');
-		e.preventDefault();
-	});
-	
-	
-	
+var myApp = angular.module('poseidon', ['ui.router']);
+
+myApp.config(function($stateProvider, $urlRouterProvider) {
+  
+  var workoutState = {
+    name: 'workout',
+    url: '/workout',
+    templateUrl: './workout-partial.html'
+  }
+
+  var loginState = {
+    name: 'login',
+    url: '/',
+    templateUrl: './login-partial.html'
+  }
+  
+  var progressState = {
+    name: 'progress',
+    url: '/progress',
+    templateUrl: './progress-partial.html'
+  }
+  
+  var settingsState = {
+    name: 'settings',
+    url: '/settings',
+    templateUrl: './settings-partial.html'
+  }
+  
+  $stateProvider.state(loginState);
+  $stateProvider.state(workoutState);
+  $stateProvider.state(progressState);
+  $stateProvider.state(settingsState);
+  
+  $urlRouterProvider.otherwise("/");
 });
