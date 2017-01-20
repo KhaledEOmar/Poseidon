330ffb4e856888ee958096aea8b05942f107c83b
Kal <komar@ncsu.edu>
2017-01-20 07:08:55 -0500

    logic added for adding and deleting workouts

--- src/js/controllers/poseidonCtrl.js
+++ src/js/controllers/poseidonCtrl.js
@@ -2,7 +2,55 @@ angular.module('poseidonControllers', ['LocalStorageModule'])
 
 	.controller('workoutCtrl', ['$scope','$http','Workouts', 'localStorageService', function($scope, $http, Workouts, localStorageService) {
 		$scope.date = new Date();
+		$scope.workouts = Workouts.getWorkouts();
+		$scope.isWeightLifting = true;
+		$scope.newWeightWorkout = {id:guid(), name: "" , weight: "", reps: ""};
+		$scope.newCardioWorkout = {id:guid(), name: "" , intensity: "", time: ""};
 		
+		$scope.formReady = false;
+		
+		$scope.switchWorkoutInput = function(){
+			if($scope.isWeightLifting == true){
+				$scope.isWeightLifting = false;
+				$scope.newWeightWorkout = {name: "" , weight: "", reps: ""};
+			}
+			else{
+				$scope.isWeightLifting = true;
+				$scope.newCardioWorkout = {name: "" , intensity: "", time: ""};
+			}
+		};
+		
+		$scope.addCardioWorkout = function(){
+			
+			Workouts.addCardioWorkout($scope.newCardioWorkout);
+			$scope.newCardioWorkout = {id:guid(), name: "" , intensity: "", time: ""};
+		};
+		
+		$scope.addWeightWorkout = function(){
+			
+			Workout.addWeightWorkout($scope.newCardioWorkout);
+			$scope.newWeightWorkout = {id:guid(), name: "" , weight: "", reps: ""};	
+		};
+		
+		$scope.deleteWeightWorkout = function(id){
+			Workouts.deleteWeightWorkout(id);
+		};
+		
+		$scope.deleteCardioWorkout = function(id){
+			Workouts.deleteCardioWorkout(id);
+		};
+		
+		
+		
+		function guid() {
+			function s4() {
+				return Math.floor((1 + Math.random()) * 0x10000)
+				.toString(16)
+				.substring(1);
+			}
+			return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+			s4() + '-' + s4() + s4() + s4();
+		}
 	}])
 		
 	.controller('progressCtrl', ['$scope','$http','Workouts', function($scope, $http, Workouts) {
--- src/js/services/poseidonService.js
+++ src/js/services/poseidonService.js
@@ -1,8 +1,42 @@
 angular.module('poseidonServices', [])
 	.factory('Workouts', function($http) {
-		return {
-			
+		var workouts = {weight: [{name:"Bench Press",weight:"180", reps:"18" , id:"0"}], cardio: [{"name":"run","intensity":"-", "time":"5",id:"0"},{"name":"walk","intensity":"8", "time":"8",id:"1"}]};
+		
+		var getWorkouts = function(){
+			return workouts;
+		};
+		
+		var deleteWeightWorkout = function(id){
+			for( var i = 0; i<workouts.weight.length; i++){ 
+				if(workouts.weight[i].id == id){
+					workouts.weight.splice(i, 1);
+				}
+			}
+		};
+		
+		var deleteCardioWorkout = function(id){
+			for( var i = 0; i<workouts.cardio.length; i++){ 
+				if(workouts.cardio[i].id == id){
+					workouts.cardio.splice(i, 1);
+				}
+			}
+		};
+		
+		var addWeightWorkout = function(workout){
+			workouts.weight.push(workout);
 		}
+		
+		var addCardioWorkout = function(workout){
+			workouts.cardio.push(workout);
+		}
+
+		return {
+			getWorkouts: getWorkouts,
+			deleteWeightWorkout:deleteWeightWorkout,
+			deleteCardioWorkout:deleteCardioWorkout,
+			addWeightWorkout:addWeightWorkout,
+			addCardioWorkout:addCardioWorkout
+		};
 	})
 	.factory('users', function($http) {
 		return {
