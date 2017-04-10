0fca0ae2f0dbf66405c77e769c8b2c4463eb5443
Khaled Omar <khaledeomar@gmail.com>
2017-04-10 03:30:01 -0400

    added facebook login

---
+++ config/config.example.js
@@ -0,0 +1,4 @@
+module.exports = {
+	fbClientID : 'APPID',
+	fbSecret: 'SECRETKEY'
+};
--- server.js
+++ server.js
@@ -1,6 +1,48 @@
 'use strict';
 
 var express = require('express');
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
+var keys = require('./config/config.js');
+
+
+
+// Configure the Facebook strategy for use by Passport.
+//
+// OAuth 2.0-based strategies require a `verify` function which receives the
+// credential (`accessToken`) for accessing the Facebook API on the user's
+// behalf, along with the user's profile.  The function must invoke `cb`
+// with a user object, which will be set at `req.user` in route handlers after
+// authentication.
+passport.use(new Strategy({
+    clientID: keys.fbClientID,
+    clientSecret: keys.fbSecret,
+    callbackURL: 'http://localhost:7000/login/facebook/return'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // In this example, the user's Facebook profile is supplied as the user
+    // record.  In a production-quality application, the Facebook profile should
+    // be associated with a user record in the application's database, which
+    // allows for account linking and authentication with other identity
+    // providers.
+    return cb(null, profile);
+  }));
+
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  In a
+// production-quality application, this would typically be as simple as
+// supplying the user ID when serializing, and querying the user record by ID
+// from the database when deserializing.  However, due to the fact that this
+// example does not have a database, the complete Facebook profile is serialized
+// and deserialized.
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+
 var app = express();
 var mongoose = require('mongoose');
 var port = process.env.PORT || 8080;        // set our port
@@ -26,8 +68,46 @@ app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
+
+
+
 // routes ======================================================================
 require('./app/routes.js')(app);
+
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Define routes.
+app.get('/',
+  function(req, res) {
+    res.render('home', { user: req.user });
+  });
+
+app.get('/login',
+  function(req, res){
+    res.render('login');
+  });
+
+app.get('/login/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/login/facebook/return', 
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/#!/workout');
+  });
+
+app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('profile', { user: req.user });
+  });
+
+
 
 // listen (start app with node server.js) ======================================
 app.listen(port);
