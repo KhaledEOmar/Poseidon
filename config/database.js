fd935c21f9089b2865e2f6dbdc2801a06e89b4ba
Khaled Omar <khaledeomar@gmail.com>
2017-04-09 23:07:01 -0400

    deleted custom user authenication code for security

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-	url : 'mongodb://poseidon:poseidon@jello.modulusmongo.net:27017/ysO5zari',
+	url : 'DATABASEURL',
 };
--- src/js/controllers/poseidonCtrl.js
+++ src/js/controllers/poseidonCtrl.js
@@ -211,29 +211,4 @@ angular.module('poseidonControllers', ['angucomplete-alt'])
 	
 	.controller('settingsCtrl', ['$scope','$http','Workouts', function($scope, $http, Workouts) {
 		
-	}])
-	
-	/*.controller('LoginController', ['$location', 'AuthenticationService', function($location, AuthenticationService){
-		var vm = this;
- 
-        vm.login = login;
- 
-        initController();
- 
-        function initController() {
-            // reset login status
-            AuthenticationService.Logout();
-        };
- 
-        function login() {
-            vm.loading = true;
-            AuthenticationService.Login(vm.username, vm.password, function (result) {
-                if (result === true) {
-                    $location.path('/');
-                } else {
-                    vm.error = 'Username or password is incorrect';
-                    vm.loading = false;
-                }
-            });
-        };
-	}])*/;
+	}]);
